9945bba187babb33eaedb790527578e91de44409
albacarmen <carmenisabelalba@gmail.com>
2024-11-13 22:10:01 -0500

    Regular topping base

---
+++ src/main/java/com/pluralsight/sandwich/RegularTopping.java
@@ -0,0 +1,13 @@
+public class RegularTopping extends Topping {
+
+    public RegularTopping(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        // Implement pricing logic for regular toppings 
+        return 0.5 * size; // Example: Price increases based on sandwich size
+    }
+}
+
