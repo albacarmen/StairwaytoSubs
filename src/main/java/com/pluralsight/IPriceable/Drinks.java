41aa55a7a34741d5514a08cff578ebae013871be
albacarmen <carmenisabelalba@gmail.com>
2024-11-13 22:05:58 -0500

    Getting started on Drinks

--- src/main/java/com/pluralsight/IPriceable/Drinks.java
+++ src/main/java/com/pluralsight/IPriceable/Drinks.java
@@ -1,4 +1,37 @@
 package com.pluralsight.IPriceable;
+public class Drink implements IPriceable {
+    private String size;
+    private String flavor;
 
-public class Drinks {
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        // Implement drink price logic 
+        double price = 1.5; // base price
+        if (size.equals("Large")) {
+            price += 1.0; // extra charge for large size
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "size='" + size + '\'' +
+                ", flavor='" + flavor + '\'' +
+                '}';
+    }
 }
+
