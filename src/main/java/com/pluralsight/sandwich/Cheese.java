9bd37e569b08990cd8af5071319658311a2dd833
albacarmen <carmenisabelalba@gmail.com>
2024-11-13 22:13:50 -0500

    Meat and Cheese setup

--- src/main/java/com/pluralsight/sandwich/Cheese.java
+++ src/main/java/com/pluralsight/sandwich/Cheese.java
@@ -1,4 +1,15 @@
 package com.pluralsight.sandwich;
 
-public class Cheese {
+public class Cheese extends PremiumTopping {
+
+    public Cheese(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        // logic for cheese
+        return 1.5 * size; //  premium price
+    }
 }
+
--- src/main/java/com/pluralsight/sandwich/Meat.java
+++ src/main/java/com/pluralsight/sandwich/Meat.java
@@ -1,4 +1,15 @@
 package com.pluralsight.sandwich;
 
-public class Meat {
+public class Meat extends PremiumTopping {
+
+    public Meat(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        // logic for meat
+        return 2.0 * size; // higher price based on size
+    }
 }
+
