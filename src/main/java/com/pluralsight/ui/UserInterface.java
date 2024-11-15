ef317272abbca063f402fbc8c7a16d7d75961d39
albacarmen <carmenisabelalba@gmail.com>
2024-11-15 01:45:13 -0500

    Starting the interface

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -7,16 +7,19 @@ import java.util.Scanner;
 public class UserInterface {
     private Order order;
     private Scanner scanner;
+    private String customerName;
 
     public UserInterface(Order order) {
         this.order = order;
         this.scanner = new Scanner(System.in);
     }
 
-    public void homeScreen() {
-        // Display main menu
-        System.out.println("Welcome to the Sandwich Shop!");
-        // Show menu options
+    // Start the interface with customer greeting
+    public void startInterface() {
+        System.out.print("Welcome to Stan Mikita's Subs! What's your name? ");
+        customerName = scanner.nextLine();
+        System.out.println("Nice to meet you, " + customerName + "! Let's get started.");
+        homeScreen();
     }
 
     public void orderScreen() {
