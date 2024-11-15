0f93de9764c2336f6509594411cf9f0a6e0b8dfb
albacarmen <carmenisabelalba@gmail.com>
2024-11-14 22:58:42 -0500

    PremiumTopping filled out

--- src/main/java/com/pluralsight/sandwich/PremiumTopping.java
+++ src/main/java/com/pluralsight/sandwich/PremiumTopping.java
@@ -3,9 +3,10 @@ package com.pluralsight.sandwich;
 public abstract class PremiumTopping extends Topping {
 
     public PremiumTopping(String type) {
-        super(type);
+        super(type); // Call the parent constructor to set the type of topping
     }
 
+    // Premium toppings will have their own specific price calculation
     @Override
     public abstract double calculatePrice(int size);
 }
