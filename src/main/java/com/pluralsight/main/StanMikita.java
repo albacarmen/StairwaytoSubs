d65f8802ee0795068e500184f8d7d7249bf6d845
albacarmen <carmenisabelalba@gmail.com>
2024-11-15 08:59:26 -0500

    I keep having errors

--- src/main/java/com/pluralsight/main/StanMikita.java
+++ src/main/java/com/pluralsight/main/StanMikita.java
@@ -10,6 +10,16 @@ public class StanMikita {
     }
 }
 
+// ORDER SUMMARY (weird looking)
+// where is txt , where does the receipt go
+
+// Checkout - display the order details and the price
+
+// check out should only have CONFIRM or Cancel
+// Confirm - create the receipt file and go back to the home screen
+// Cancel - delete order and go back to the home screen
+// When exiting, it shows the whole menu again, just show 1. new order or exit
+
 
 
 
